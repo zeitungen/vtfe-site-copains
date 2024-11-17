d2757bb8c8a64acb7f8006761c83f92f5436c062
Maxime Journaux <maxime.journaux@espridigital.fr>
2024-11-17 14:51:30 +0100

    style

--- scripts.js
+++ scripts.js
@@ -1,10 +1,44 @@
 const quotes = [
     { text: "Ce n'est pas la biquette la plus vierge du troupeau.", author: "Loïc" },
-    { text: "On a tous un peu de blond dans l'aile", author: "Elise"}
+    { text: "On a tous un peu de blond dans l'aile", author: "Elise" },
+
+    { text: "C'est pas parce qu'on a rien à dire qu'il faut fermer sa gueule.", author: "Loïc" },
+    { text: "Je suis pas un pervers, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un pervers, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un pervers, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un pervers, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un pervers, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un homme, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un homme, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un homme, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un schtroumpf, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un schtroumpf, je suis un artiste.", author: "Loïc" },
+    { text: "Je suis pas un schtroumpf, je suis un artiste.", author: "Loïc" },
 ];
 
+
 document.addEventListener('DOMContentLoaded', () => {
+    const quoteElement = document.getElementById('quote');
+    const authorElement = document.getElementById('author');
+    const allQuotesElement = document.getElementById('all-quotes');
+    const toggleButton = document.getElementById('toggle-button');
+
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    document.getElementById('quote').textContent = randomQuote.text;
-    document.getElementById('author').textContent = randomQuote.author;
+    quoteElement.textContent = randomQuote.text;
+    authorElement.textContent = randomQuote.author;
+
+    toggleButton.addEventListener('click', () => {
+        if (allQuotesElement.style.display === 'none') {
+            allQuotesElement.style.display = 'block';
+            quoteElement.style.display = 'none';
+            authorElement.style.display = 'none';
+            toggleButton.textContent = 'Afficher une citation aléatoire';
+            allQuotesElement.innerHTML = quotes.map(quote => `<p>${quote.text} <cite>${quote.author}</cite></p>`).join('');
+        } else {
+            allQuotesElement.style.display = 'none';
+            quoteElement.style.display = 'block';
+            authorElement.style.display = 'block';
+            toggleButton.textContent = 'Afficher toutes les citations';
+        }
+    });
 });
